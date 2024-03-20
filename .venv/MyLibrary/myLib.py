8d8c1e1ffaeb3ae3c866cb3aa5905f7e781c1bd7
Farik9G <92394604+Farik9G@users.noreply.github.com>
2024-03-20 20:41:18 +0300

    Update testCircle

--- .venv/MyLibrary/myLib.py
+++ .venv/MyLibrary/myLib.py
@@ -1,2 +1,21 @@
-def calculateSquare(*args):
-    return
+from abc import ABC, abstractmethod
+
+class Figure(ABC):
+    @abstractmethod
+    def square(self):
+        pass
+
+class Circle(Figure):
+    def __init__(self, radius):
+        self.radius = radius
+    def square(self):
+        return 0
+
+class Triangle(Figure):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def square(self):
+        return 0
--- .venv/Tests/testCircle.py
+++ .venv/Tests/testCircle.py
@@ -1,26 +1,46 @@
 import unittest
-from myLib import calculateSquare
 from math import pi
+from MyLibrary.myLib import Circle
 
 class TestCircleCalc(unittest.TestCase):
     def testArea(self):
-        self.assertEqual(calculateSquare(8), pi*64)
-        self.assertEqual(calculateSquare(7.5), pi*7.5*7.5)
-        self.assertEqual(calculateSquare(1), pi)
-        self.assertEqual(calculateSquare(0), 0)
+        circle1 = Circle(8)
+        self.assertAlmostEqual(circle1.square(), pi*64, places=5)
+
+        circle2 = Circle(7.5)
+        self.assertAlmostEqual(circle2.square(), pi*7.5*7.5, places=5)
+
+        circle3 = Circle(1)
+        self.assertEqual(circle3.square(), pi)
+
+        circle4 = Circle(0)
+        self.assertEqual(circle4.square(), 0)
+
     def testValues(self):
-        self.assertRaises(ValueError, calculateSquare, -5)
-        self.assertRaises(ValueError, calculateSquare, -10)
-        self.assertRaises(ValueError, calculateSquare, -11.637)
+        with self.assertRaises(ValueError):
+            Circle(-5)
+        with self.assertRaises(ValueError):
+            Circle(-10)
+        with self.assertRaises(ValueError):
+            Circle(-11.637)
 
     def testTypes(self):
-        self.assertRaises(TypeError, calculateSquare, [117, 32, 17])
-        self.assertRaises(TypeError, calculateSquare, [13])
-        self.assertRaises(TypeError, calculateSquare, 'radius')
-        self.assertRaises(TypeError, calculateSquare, (5, 6))
-        self.assertRaises(TypeError, calculateSquare, (9))
-        self.assertRaises(TypeError, calculateSquare, True)
-        self.assertRaises(TypeError, calculateSquare, 13+2j)
-        self.assertRaises(TypeError, calculateSquare, {6.5})
-        self.assertRaises(TypeError, calculateSquare, {1: '42'})
+        with self.assertRaises(TypeError):
+            Circle([117, 32, 17])
+        with self.assertRaises(TypeError):
+            Circle([13])
+        with self.assertRaises(TypeError):
+            Circle('radius')
+        with self.assertRaises(TypeError):
+            Circle((5, 6))
+        with self.assertRaises(TypeError):
+            Circle((9))
+        with self.assertRaises(TypeError):
+            Circle(True)
+        with self.assertRaises(TypeError):
+            Circle(13+2j)
+        with self.assertRaises(TypeError):
+            Circle({6.5})
+        with self.assertRaises(TypeError):
+            Circle({1: '42'})
 
