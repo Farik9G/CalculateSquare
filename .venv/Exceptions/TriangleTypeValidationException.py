2cb9264eb2f7f336e0acf75593148979e3e82a33
Farik9G <92394604+Farik9G@users.noreply.github.com>
2024-03-21 13:50:14 +0300

    Add lib and exceptions to master

---
+++ .venv/Exceptions/TriangleTypeValidationException.py
@@ -0,0 +1,11 @@
+class TriangleTypeError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+    def __str__(self):
+        if self.message:
+            return 'TriangleTypeError: {0} '.format(self.message)
+        else:
+            return 'TriangleTypeError has been raised'
